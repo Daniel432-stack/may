f32428e6dfae7918b2ca4d07d7570dea339feb3b
Daniel432-stack <danielshermmuhammadov@gmail.com>
2025-05-27 09:57:10 +0600

    may

---
+++ python.py
@@ -0,0 +1,17 @@
+баа = int(input("Бааңды киргиз: "))
+
+if баа > 90:
+    print("Отлично!")
+elif баа >= 70:
+    print("Хорошо!")
+elif баа >= 50:
+    print("Средне")
+else:
+    print("Учись больше!")
+
+# 2 Exemple
+
+print(10 + 5)
+print(10 - 5)
+print(10 * 5)
+print(10 / 5)
